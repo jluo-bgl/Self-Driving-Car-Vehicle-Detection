3e6eb858a37bec8b3ca3afe971645ebb22b15f59
James Luo <jluo@bglcorp.com.au>
2017-06-24 18:57:00 +1000

    remove unused code

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from moviepy.editor import VideoFileClip
-from vehicle_detect_nn import VehicleDetector
 from lane_finder import LaneFinder
 from object_detect_yolo import YoloDetector
 
@@ -8,13 +7,12 @@ if __name__ == "__main__":
     def remove_mp4_extension(file_name):
         return file_name.replace(".mp4", "")
 
-    # detector = VehicleDetector(img_rows=640, img_cols=960, weights_file="model_segn_small_0p72.h5")
-    # lane_finder = LaneFinder(save_original_images=False, object_detection_mask=detector.get_Unet_mask)
     yolo = YoloDetector()
     lane_finder = LaneFinder(save_original_images=True, object_detection_func=yolo.process_image_array)
     video_file = 'project_video.mp4'
     # video_file = 'challenge_video.mp4'
     # video_file = 'back_home.mov'
+    # video_file = 'file01_2017322191247.mp4'
     clip = VideoFileClip(video_file, audio=False)
     t_start = 0
     t_end = 0
